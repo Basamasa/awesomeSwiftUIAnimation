2b8bacac06b60f8bba9bbb8418e78c42a8289379
Anzer Arkin <anzerbasamasa@gmail.com>
2023-02-25 19:01:47 +0100

    More animations

---
+++ AnimationMacApp/RoundAnimationView.swift
@@ -0,0 +1,117 @@
+//
+//  RoundAnimationView.swift
+//  AnimationMacApp
+//
+//  Created by Anzer Arkin on 25.02.23.
+//
+
+import SwiftUI
+
+struct RoundAnimationView: View {
+    
+    @State private var height: CGFloat = 30
+    @State private var angleX: Double = 0
+    @State private var angleY: Double = 0
+    
+    let numberOfRectangles: Int = 60
+    let timeBetweenAnimations: Double = 1.5
+    let angleAmount: Double = 10
+    
+    var width: CGFloat {
+        CGFloat(360 / numberOfRectangles) * 0.9
+    }
+    
+    var animation: Animation {
+        .easeInOut(duration: timeBetweenAnimations)
+    }
+    
+    var body: some View {
+        ZStack {
+            Image(systemName: "figure.mind.and.body")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundColor(.gray)
+                .opacity(0.3)
+                .rotation3DEffect(.degrees(angleX), axis: (x: 1, y: 0, z: 0))
+                .rotation3DEffect(.degrees(angleY), axis: (x: 0, y: 1, z: 0))
+            
+            ForEach(0 ..< numberOfRectangles) { i in
+                Rectangle(
+                    index: i,
+                    width: width,
+                    numberOfRectangles: numberOfRectangles
+                )
+            }
+        }
+        .onAppear {
+            withAnimation(animation) {
+                angleX = -angleAmount
+                angleY = -angleAmount
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + timeBetweenAnimations) {
+                withAnimation(animation) {
+                    angleX = angleAmount
+                    angleY = -angleAmount
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + timeBetweenAnimations) {
+                    withAnimation(animation) {
+                        angleX = angleAmount
+                        angleY = angleAmount
+                    }
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + timeBetweenAnimations) {
+                        withAnimation(animation) {
+                            angleX = -angleAmount
+                            angleY = angleAmount
+                        }
+                    }
+                }
+            }
+            
+            
+        }
+    }
+}
+
+struct Rectangle: View {
+    @State private var height: CGFloat = 10
+    @State private var opacity: Double = 0.4
+    @State private var color: Color = .gray
+    
+    var index: Int
+    var width: CGFloat
+    var numberOfRectangles: Int
+    
+    var delay: Double {
+        print(Double(index) * 0.1)
+        return Double(index) * 0.1
+    }
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 4, style: .continuous)
+            .fill(color)
+            .frame(width: width, height: height, alignment: .center)
+            .animation(.easeInOut.delay(delay), value: height)
+            .opacity(opacity)
+            .animation(.spring().delay(delay), value: opacity)
+            .offset(y: 100)
+            .rotationEffect(.degrees(degrees(for: index)), anchor: .center)
+            .onAppear {
+                height = 30
+                opacity = 0.7
+                color = .green
+            }
+    }
+    
+    func degrees(for index: Int) -> Double {
+        Double((index+1) * (360 / numberOfRectangles))
+    }
+}
+
+struct RoundAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundAnimationView()
+    }
+}
---
+++ AnimationMacApp/TextChangeView.swift
@@ -0,0 +1,51 @@
+//
+//  TextChangeView.swift
+//  AnimationMacApp
+//
+//  Created by Anzer Arkin on 25.02.23.
+//
+
+import SwiftUI
+
+struct TextChangeView: View {
+    @State private var isRotating = false
+    @State private var isHidden = false
+    
+    @Environment(\.accessibilityReduceMotion) var reduceMotion
+    
+    var body: some View {
+        VStack(spacing: 14){
+            Rectangle() // top
+                .frame(width: 64, height: 10)
+                .cornerRadius(4)
+                .rotationEffect(.degrees(isRotating ? 47 : 0), anchor: .leading)
+            
+            Rectangle() // middle
+                .frame(width: 64, height: 10)
+                .cornerRadius(4)
+                .scaleEffect(isHidden ? 0 : 1, anchor: isHidden ? .trailing: .leading)
+                .opacity(isHidden ? 0 : 1)
+            
+            Rectangle() // bottom
+                .frame(width: 64, height: 10)
+                .cornerRadius(4)
+                .rotationEffect(.degrees(isRotating ? -47 : 0), anchor: .leading)
+        }
+        .accessibilityElement(children: .combine)
+        .accessibilityAddTraits(.isButton)
+        .accessibilityLabel("Hamburger menu")
+        .onTapGesture {
+            withAnimation(reduceMotion ? .easeInOut : .interpolatingSpring(stiffness: 300, damping: 15)){
+                isRotating.toggle()
+                isHidden.toggle()
+            }
+        }
+        
+    }
+}
+
+struct TextChangeView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextChangeView()
+    }
+}
